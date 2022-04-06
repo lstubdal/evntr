281f4603d28f0b4abe35331728b5470d92dc25c7
lstubdal <lotte.stubdal@gmail.com>
2022-04-06 11:51:05 +0200

    Change to object

---
+++ studio/schemas/documents/address.js
@@ -0,0 +1,17 @@
+export default {
+    type: 'Address',
+    name: 'address',
+    type: 'document',
+    fields: [
+        {
+            title: 'Street Address',
+            name: 'streetAddress',
+            type: 'string'
+        },
+        {
+            title: 'City',
+            name: 'city',
+            type: 'string'
+        }
+    ]
+}
