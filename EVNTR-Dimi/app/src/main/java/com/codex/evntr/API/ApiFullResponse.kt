d9b4a031b5e90029abaaca945cfee5247bc386e0
Den EKTESTE MFGen du finner <99316570+ektemfg@users.noreply.github.com>
2022-04-07 03:23:56 +0200

    Delete EVNTR-Dimi directory

--- EVNTR-Dimi/app/src/main/java/com/codex/evntr/API/ApiFullResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.codex.evntr.API
-
-import com.codex.evntr.database.Event
-
-data class ApiFullResponse(
-    val ms: Int?,
-    val query: String?,
-    val result: List<fetchedEvent>?
-)
